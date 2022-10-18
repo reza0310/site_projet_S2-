cf5ddb966a1da95f540459ec14eb9a2a9c8b6818
reza0310 <michelageou@gmail.com>
2022-10-18 22:33:35 +0200

    Site terminé

---
+++ lore/index.php
@@ -0,0 +1,4 @@
+<?php
+set_include_path($_SERVER['DOCUMENT_ROOT']."site_s2");
+echo(str_replace("tres", "active", str_replace("%php%", file_get_contents("page.html"), file_get_contents("header.html", true))));
+?>
