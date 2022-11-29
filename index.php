07782e4a3f99b13bbc3c9ebbac635cedc4ba996c
root <root@odaame.org>
2022-11-29 12:08:53 +0100

    Fixed downloads

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-set_include_path($_SERVER['DOCUMENT_ROOT']."site_s2");
+set_include_path($_SERVER['DOCUMENT_ROOT']."/S2");
 echo(str_replace("uno", "active", str_replace("%php%", file_get_contents("page.html"), file_get_contents("header.html", true))));
 ?>
---
+++ telechargements/index.php
@@ -0,0 +1,4 @@
+<?php
+set_include_path($_SERVER['DOCUMENT_ROOT']."/S2");
+echo(str_replace("octo", "active", str_replace("%php%", file_get_contents("page.html"), file_get_contents("header.html", true))));
+?>
